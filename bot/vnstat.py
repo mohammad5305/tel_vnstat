2c50f1372a8c4b9af1c8e948438cd84f95e6024e
mohammad5305 <mohammad.bajalal546@gmail.com>
2023-12-17 15:12:43 +0330

    refactor: remove useless variables

--- bot/vnstat.py
+++ bot/vnstat.py
@@ -1,40 +1,48 @@
 import json
-import os
 import subprocess
-import datetime
 import calendar
 
-
-def vnstat_this_month_usage(interface_name):
+def month_usage(interface_name: str) -> tuple[str, ...]:
+    """Get this month data from vnstat json"""
     stdout = subprocess.getoutput(f"vnstat -s -i {interface_name} --json m")
-    # with open('vnstat_output.json', 'r') as f:
-    #     stdout = f.read()
-    j = json.loads(stdout)
-    interface = j['interfaces'][0]
-    interface_name = interface['name']
-    month = j['interfaces'][0]['traffic']['month'][-1]
-    rx = month['rx']
-    tx = month['tx']
-    total = month['rx'] + month['tx']
-    month_name = calendar.month_name[month['date']['month']]
-    return interface_name, rx, tx, total, month_name
+    if "error:" in stdout.lower():
+        raise RuntimeError(stdout)
+
+    data = json.loads(stdout)
 
 
-def human_bytes(B):
-    """Return the given bytes as a human friendly KB, MB, GB, or TB string."""
-    B = float(B)
+    interface = data['interfaces'][0]
+    month = interface['traffic']['month'][-1]
+    month_name = str(calendar.month_name[month['date']['month']])
+
+    return (interface['name'],
+            month['rx'],
+            month['tx'],
+            month['rx'] + month['tx'],
+            month_name)
+
+
+def human_bytes(B: float) -> str:
+    """Return the given bytes as a human friendly KB, MB, GB, or TB string"""
     KB = float(1024)
     MB = float(KB ** 2)  # 1,048,576
     GB = float(KB ** 3)  # 1,073,741,824
     TB = float(KB ** 4)  # 1,099,511,627,776
 
-    if B < KB:
-        return '{0} {1}'.format(B,'Bytes' if 0 == B > 1 else 'Byte')
-    elif KB <= B < MB:
-        return '{0:.2f} KB'.format(B / KB)
+    if KB <= B < MB:
+        B = B / KB
+        prefix = "KB"
     elif MB <= B < GB:
-        return '{0:.2f} MB'.format(B / MB)
+        B = B / MB
+        prefix = "MB"
     elif GB <= B < TB:
-        return '{0:.2f} GB'.format(B / GB)
+        B = B / GB
+        prefix = "GB"
     elif TB <= B:
-        return '{0:.2f} TB'.format(B / TB)
+        B = B / TB
+        prefix = "GB"
+    else:
+        prefix = "Bytes"
+
+    return f"{B:.2f} {prefix}"
+
